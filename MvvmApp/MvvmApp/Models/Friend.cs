26eb5be40d5d70bb20d793291e9339f23d28d10f
Sander Demihhovski <demihhovski@mail.ru>
2021-04-16 11:22:10 +0300

    Добавьте файлы проекта.

---
+++ MvvmApp/MvvmApp/Models/Friend.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MvvmApp.Models
+{
+    public class Friend
+    {
+        public string Name { get; set; }
+        public string Second { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Birthday { get; set; }
+        public string Age { get; set; }
+        public string Work { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ MvvmApp/MvvmApp/ViewModels/FriendViewModel.cs
@@ -0,0 +1,149 @@
+﻿using MvvmApp.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace MvvmApp.ViewModels
+{
+    public class FriendViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        FriendsListViewModel lvm;
+
+        public Friend Friend { get; private set; }
+
+        public FriendViewModel()
+        {
+            Friend = new Friend();
+        }
+
+        public FriendsListViewModel ListViewModel
+        {
+            get { return lvm; }
+            set
+            {
+                if (lvm != value)
+                {
+                    lvm = value;
+                    OnPropertyChanged("ListViewModel");
+                }
+            }
+        }
+        public string Name
+        {
+            get { return Friend.Name; }
+            set
+            {
+                if (Friend.Name != value)
+                {
+                    Friend.Name = value;
+                    OnPropertyChanged("Name");
+                }
+            }
+        }
+        public string Second
+        {
+            get { return Friend.Second; }
+            set
+            {
+                if (Friend.Second != value)
+                {
+                    Friend.Second = value;
+                    OnPropertyChanged("Second");
+                }
+            }
+        }
+        public string Email
+        {
+            get { return Friend.Email; }
+            set
+            {
+                if (Friend.Email != value)
+                {
+                    Friend.Email = value;
+                    OnPropertyChanged("Email");
+                }
+            }
+        }
+        public string Phone
+        {
+            get { return Friend.Phone; }
+            set
+            {
+                if (Friend.Phone != value)
+                {
+                    Friend.Phone = value;
+                    OnPropertyChanged("Phone");
+                }
+            }
+        }
+        public string Birthday
+        {
+            get { return Friend.Birthday; }
+            set
+            {
+                if (Friend.Birthday != value)
+                {
+                    Friend.Birthday = value;
+                    OnPropertyChanged("Birthday");
+                }
+            }
+        }
+        public string Age
+        {
+            get { return Friend.Age; }
+            set
+            {
+                if (Friend.Age != value)
+                {
+                    Friend.Age = value;
+                    OnPropertyChanged("Age");
+                }
+            }
+        }
+        public string Work
+        {
+            get { return Friend.Work; }
+            set
+            {
+                if (Friend.Work != value)
+                {
+                    Friend.Work = value;
+                    OnPropertyChanged("Work");
+                }
+            }
+        }
+        public string Description
+        {
+            get { return Friend.Description; }
+            set
+            {
+                if (Friend.Description != value)
+                {
+                    Friend.Description = value;
+                    OnPropertyChanged("Description");
+                }
+            }
+        }
+
+        public bool IsValid
+        {
+            get
+            {
+                return !string.IsNullOrEmpty(Name.Trim()) ||
+                    !string.IsNullOrEmpty(Second.Trim()) ||
+                    !string.IsNullOrEmpty(Phone.Trim()) ||
+                    !string.IsNullOrEmpty(Email.Trim()) ||
+                    !string.IsNullOrEmpty(Birthday.Trim()) ||
+                    !string.IsNullOrEmpty(Age.Trim()) ||
+                    !string.IsNullOrEmpty(Work.Trim());
+            }
+        }
+        protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
